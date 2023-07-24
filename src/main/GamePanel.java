59b5a5395ac6f9117d40b91518522c9bb0a3af20
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-07-24 11:16:08 +0200

    Key handler & basic movements

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,13 +15,21 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCols;
     final int screenHeight = tileSize * maxScreenRows;
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //set player default pos
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 5;
 
     public GamePanel()
     {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread()
@@ -40,9 +48,21 @@ public class GamePanel extends JPanel implements Runnable{
         }
     }
 
-    public void update()
-    {
-
+    public void update() {
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        if (keyH.rightPressed == true)
+        {
+            playerX += playerSpeed;
+        }
+        if (keyH.leftPressed == true)
+        {
+            playerX -= playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g)
@@ -52,7 +72,7 @@ public class GamePanel extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100,100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 }
